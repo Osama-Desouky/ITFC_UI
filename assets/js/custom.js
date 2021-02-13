08b1915b7641f4a7fe1d6dc34a12db238b604ae4
Mohsen Galal <devmohsengalal@gmail.com>
2021-02-13 12:31:21 +0200

    navigator

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,18 @@
 "use strict";
 
-var scrollTo = function (sectionEl="") {
+var scrollTo = function (sectionEl = "", callback = function () {}) {
   $("html, body").animate(
     {
-      scrollTop: sectionEl?$(sectionEl).offset().top - 69:0,
+      scrollTop: sectionEl ? calculateOffset(sectionEl) : 0,
     },
-    800
+    800,
+    "swing",
+    callback()
   );
+};
+var calculateOffset = function (el) {
+  if ($(window).width() >= 980) return Math.floor($(el).offset().top - 69);
+  return Math.floor($(el).offset().top);
 };
 // on ready state
 $(function () {
@@ -47,6 +53,27 @@ $(function () {
         $("body").css({ "padding-top": "0px" });
       }
     }
+    if (scroll >= calculateOffset(".in-focus"))
+      $("#navFocus").addClass("active");
+    else $("#navFocus").removeClass("active");
+
+    if (scroll >= calculateOffset(".latest-news"))
+      $("#navNews").addClass("active");
+    else $("#navNews").removeClass("active");
+    if (scroll >= calculateOffset(".upcoming-events"))
+      $("#navEvents").addClass("active");
+    else $("#navEvents").removeClass("active");
+    if (scroll >= calculateOffset(".publications"))
+      $("#navPublications").addClass("active");
+    else $("#navPublications").removeClass("active");
+    if (
+      scroll >= calculateOffset(".our-group") ||
+      $(window).scrollTop() + $(window).height() == $(document).height()
+    )
+      $("#navISDB").addClass("active");
+    else $("#navISDB").removeClass("active");
+
+    console.log(scroll, calculateOffset(".publications"));
   });
   $(".search-toggle").on("click", function () {
     $(".search-box").toggle();
@@ -56,5 +83,33 @@ $(function () {
   });
   $(".scroll-up").on("click", function () {
     scrollTo();
+  });
+  $("#navIntro a").on("click", function () {
+    scrollTo();
+  });
+  $("#navFocus a").on("click", function () {
+    scrollTo(".in-focus", function () {
+      $("#navFocus").addClass("active");
+    });
+  });
+  $("#navNews a").on("click", function () {
+    scrollTo(".latest-news", function () {
+      $("#navNews").addClass("active");
+    });
+  });
+  $("#navEvents a").on("click", function () {
+    scrollTo(".upcoming-events", function () {
+      $("#navEvents").addClass("active");
+    });
+  });
+  $("#navPublications a").on("click", function () {
+    scrollTo(".publications", function () {
+      $("#navPublications").addClass("active");
+    });
+  });
+  $("#navISDB a").on("click", function () {
+    scrollTo(".our-group", function () {
+      $("#navISDB").addClass("active");
+    });
   });
 });
